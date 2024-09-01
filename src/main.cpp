d0896a6fbd323233bf66bc37d9943ddc1a81a78d
Coffe <jonathan_davey@outlook.com>
2024-09-01 19:29:42 +1000

    Le sand and water game

--- src/main.cpp
+++ src/main.cpp
@@ -2,62 +2,153 @@
 
 #include "SFML/Graphics.hpp"
 
-class Pixel : public sf::Vertex
+#define SCREEN_X 400
+#define SCREEN_Y 240
+
+typedef enum
+{
+    AIR,
+    WATER,
+    SAND,
+} cell_t;
+
+class CellData
 {
 public:
-    Pixel(sf::Vector2f pos, sf::Color color)
-        : Vertex(pos, color)
-    {}
+    cell_t type = AIR;
 };
+
+void handle_events(sf::RenderWindow &window, std::vector<CellData>& point_data)
+{
+    static bool mouse_pressed = false;
+
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        switch (event.type)
+        {
+        case sf::Event::Closed:
+            window.close();
+            break;
+        default:
+            break;
+        }
+    }
+
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    {
+        auto mp = sf::Mouse::getPosition(window);
+        point_data[mp.x + mp.y * SCREEN_X].type = SAND;
+    }
+
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
+    {
+        auto mp = sf::Mouse::getPosition(window);
+        point_data[mp.x + mp.y * SCREEN_X].type = WATER;
+    }
+}
+
+bool try_move_to(std::vector<CellData>& point_data, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
+{
+    if (x2 < 0 || x2 >= SCREEN_X || y2 < 0 || y2 >= SCREEN_Y)
+    {
+        return false;
+    }
+    if (point_data[x2 + y2 * SCREEN_X].type < point_data[x1 + y1 * SCREEN_X].type)
+    {
+        auto tmp = point_data[x1 + y1 * SCREEN_X].type;
+        point_data[x1 + y1 * SCREEN_X].type = point_data[x2 + y2 * SCREEN_X].type;
+        point_data[x2 + y2 * SCREEN_X].type = tmp;
+        return true;
+    }
+    return false;
+}
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(400, 240), "Test Window");
+    sf::RenderWindow window(sf::VideoMode(SCREEN_X, SCREEN_Y), "Cells");
     window.setFramerateLimit(60);
 
-    sf::VertexArray pointmap(sf::Points, 400 * 240);
-    std::vector<Pixel> pixels;
+    sf::VertexArray points(sf::Points, SCREEN_X * SCREEN_Y);
+    std::vector<CellData> point_data(SCREEN_X * SCREEN_Y);
 
-    for (uint32_t i = 0; i < 400 * 240; i++)
+    srand(clock());
+
+    // Initial state
+    for (uint32_t i = 0; i < SCREEN_X * SCREEN_Y; i++)
     {
-        Pixel px(sf::Vector2f(i % 400, i / 400), i % 2 ? sf::Color::Green : sf::Color::Blue);
-        pixels.push_back(px);
-        // pointmap[i].position = sf::Vector2f(i % 400, i / 400);
-        // if (i % 2) pointmap[i].color = sf::Color::Green;
-        // else pointmap[i].color = sf::Color::Blue;
+        points[i].position = sf::Vector2f(i % 400, i / 400);
+
+        point_data[i].type = i % 3 ? WATER : AIR;
     }
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        handle_events(window, point_data);
+
+        // Update state
+        for (auto y = SCREEN_Y - 1; y >= 0; y--)
         {
-            switch (event.type)
+            for (auto x = SCREEN_X - 1; x >= 0; x--)
             {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::MouseButtonPressed:
-            {
-                auto mp = sf::Mouse::getPosition(window);
-                std::cout << "Vec " << mp.x << "," << mp.y << "\n";
-                break;
+                switch (point_data[x + y * SCREEN_X].type)
+                {
+                case AIR:
+                    break;
+                case SAND:
+                    if (try_move_to(point_data, x, y, x + 0, y + 1)) break;
+                    if (rand() % 2)
+                    {
+                        if (try_move_to(point_data, x, y, x + 1, y + 1)) break;
+                        if (try_move_to(point_data, x, y, x - 1, y + 1)) break;
+                    } else
+                    {
+                        if (try_move_to(point_data, x, y, x - 1, y + 1)) break;
+                        if (try_move_to(point_data, x, y, x + 1, y + 1)) break;
+                    }
+                    break;
+                case WATER:
+                    if (try_move_to(point_data, x, y, x + 0, y + 1)) break;
+                    if (rand() % 2)
+                    {
+                        if (try_move_to(point_data, x, y, x + 1, y + 1)) break;
+                        if (try_move_to(point_data, x, y, x - 1, y + 1)) break;
+                        if (try_move_to(point_data, x, y, x + 1, y + 0)) break;
+                        if (try_move_to(point_data, x, y, x - 1, y + 0)) break;
+                    } else
+                    {
+                        if (try_move_to(point_data, x, y, x - 1, y + 1)) break;
+                        if (try_move_to(point_data, x, y, x + 1, y + 1)) break;
+                        if (try_move_to(point_data, x, y, x - 1, y + 0)) break;
+                        if (try_move_to(point_data, x, y, x + 1, y + 0)) break;
+                    }
+                    break;
+                default:
+                    break;
+                }
             }
+        }
+
+        // Draw state
+        window.clear(sf::Color::Black);
+        for (auto i = 0; i < SCREEN_X * SCREEN_Y; i++)
+        {
+            switch (point_data[i].type)
+            {
+            case AIR:
+                points[i].color = sf::Color::Black;
+                break;
+            case SAND:
+                points[i].color = sf::Color::Yellow;
+                break;
+            case WATER:
+                points[i].color = sf::Color::Blue;
+                break;
             default:
                 break;
             }
         }
-
-        window.clear(sf::Color::Black);
-        // window.draw(pointmap);
-
-
-        // Each pixel is drawn individually..
-        // This is inefficient but we will deal with it when it's a problem
-        for (auto & px : pixels)
-        {
-            window.draw(&px, 1, sf::Points, sf::RenderStates::Default);
-        }
+        window.draw(points);
 
         window.display();
     }

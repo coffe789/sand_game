cc6ff9643854a8e5d22f2e6f9f1953cfe3ac7e5d
Coffe <jonathan_davey@outlook.com>
2024-09-01 14:55:43 +1000

    More tutorial following..

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 void handle_input(sf::RenderWindow &window)
@@ -36,21 +37,50 @@ void handle_input(sf::RenderWindow &window)
     }
 }
 
-void update_frame(sf::RenderWindow& window)
+void init_shape(sf::RectangleShape& shape, sf::Vector2f const&pos, sf::Color const& color)
 {
-    window.clear(sf::Color::Black);
+    shape.setFillColor(color);
+    shape.setPosition(pos);
+    shape.setOrigin(shape.getSize() * 0.5f);
 }
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(300, 200), "The title");
+    sf::RenderWindow window(sf::VideoMode(480, 180), "The title");
+    window.setFramerateLimit(60);
+
+    sf::Vector2f startPos = sf::Vector2f(50, 50);
+    sf::RectangleShape playerRect(sf::Vector2f(50, 50));
+    init_shape(playerRect, startPos, sf::Color::Green);
+    sf::RectangleShape targetRect(sf::Vector2f(50, 50));
+    init_shape(targetRect, sf::Vector2f(400, 50), sf::Color::Blue);
+    sf::RectangleShape badRect(sf::Vector2f(50, 100));
+    init_shape(badRect, sf::Vector2f(250, 50), sf::Color::Red);
+
 
     // Game loop
     while (window.isOpen())
     {
+        // Input handling
         handle_input(window);
 
-        update_frame(window);
+        // Update frame
+        playerRect.move(1, 0);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+            playerRect.move(0, 1);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+            playerRect.move(0, -1);
+
+        if (playerRect.getGlobalBounds().intersects((targetRect.getGlobalBounds())))
+            window.close();
+        if (playerRect.getGlobalBounds().intersects((badRect.getGlobalBounds())))
+            playerRect.setPosition(startPos);
+
+        // Render frame
+        window.clear(sf::Color::Black);
+        window.draw(playerRect);
+        window.draw(targetRect);
+        window.draw(badRect);
 
         window.display();
     }

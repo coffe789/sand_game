4953b5b9bff0b6ba7c81e649fbf2b62ba5e811a8
Coffe <jonathan_davey@outlook.com>
2024-09-02 22:57:30 +1000

    Mess around with viewports

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 
 #include "SFML/Graphics.hpp"
 
-#define SCREEN_X 400
-#define SCREEN_Y 240
+#define WINDOW_X 800
+#define WINDOW_Y 480
+
+#define SANDBOX_X 400
+#define SANDBOX_Y 400
 
 typedef enum
 {
@@ -38,27 +41,27 @@ void handle_events(sf::RenderWindow &window, std::vector<CellData>& point_data)
     if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
     {
         auto mp = sf::Mouse::getPosition(window);
-        point_data[mp.x + mp.y * SCREEN_X].type = SAND;
+        point_data[mp.x + mp.y * SANDBOX_X].type = SAND;
     }
 
     if (sf::Mouse::isButtonPressed(sf::Mouse::Right))
     {
         auto mp = sf::Mouse::getPosition(window);
-        point_data[mp.x + mp.y * SCREEN_X].type = WATER;
+        point_data[mp.x + mp.y * SANDBOX_X].type = WATER;
     }
 }
 
 bool try_move_to(std::vector<CellData>& point_data, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
 {
-    if (x2 < 0 || x2 >= SCREEN_X || y2 < 0 || y2 >= SCREEN_Y)
+    if (x2 < 0 || x2 >= SANDBOX_X || y2 < 0 || y2 >= SANDBOX_Y)
     {
         return false;
     }
-    if (point_data[x2 + y2 * SCREEN_X].type < point_data[x1 + y1 * SCREEN_X].type)
+    if (point_data[x2 + y2 * SANDBOX_X].type < point_data[x1 + y1 * SANDBOX_X].type)
     {
-        auto tmp = point_data[x1 + y1 * SCREEN_X].type;
-        point_data[x1 + y1 * SCREEN_X].type = point_data[x2 + y2 * SCREEN_X].type;
-        point_data[x2 + y2 * SCREEN_X].type = tmp;
+        auto tmp = point_data[x1 + y1 * SANDBOX_X].type;
+        point_data[x1 + y1 * SANDBOX_X].type = point_data[x2 + y2 * SANDBOX_X].type;
+        point_data[x2 + y2 * SANDBOX_X].type = tmp;
         return true;
     }
     return false;
@@ -66,20 +69,25 @@ bool try_move_to(std::vector<CellData>& point_data, uint16_t x1, uint16_t y1, ui
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(SCREEN_X, SCREEN_Y), "Cells");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_X, WINDOW_Y), "Cells");
     window.setFramerateLimit(60);
 
-    sf::VertexArray points(sf::Points, SCREEN_X * SCREEN_Y);
-    std::vector<CellData> point_data(SCREEN_X * SCREEN_Y);
+    auto w_size = window.getSize();
+    sf::View view(sf::Vector2f(w_size.x / 2, w_size.y / 2), sf::Vector2f(w_size.x, w_size.y));
+    view.setViewport(sf::FloatRect(0.05f, 0.05f, 1.0f, 1.0f));
+    window.setView(view);
+
+    sf::VertexArray points(sf::Points, SANDBOX_X * SANDBOX_Y);
+    std::vector<CellData> point_data(SANDBOX_X * SANDBOX_Y);
 
     srand(clock());
 
     // Initial state
-    for (uint32_t i = 0; i < SCREEN_X * SCREEN_Y; i++)
+    for (uint32_t i = 0; i < SANDBOX_X * SANDBOX_Y; i++)
     {
         points[i].position = sf::Vector2f(i % 400, i / 400);
 
-        point_data[i].type = i % 3 ? WATER : AIR;
+        point_data[i].type = i % 3 ? WATER : WATER;
     }
 
     while (window.isOpen())
@@ -87,11 +95,11 @@ int main()
         handle_events(window, point_data);
 
         // Update state
-        for (auto y = SCREEN_Y - 1; y >= 0; y--)
+        for (auto y = SANDBOX_Y - 1; y >= 0; y--)
         {
-            for (auto x = SCREEN_X - 1; x >= 0; x--)
+            for (auto x = SANDBOX_X - 1; x >= 0; x--)
             {
-                switch (point_data[x + y * SCREEN_X].type)
+                switch (point_data[x + y * SANDBOX_X].type)
                 {
                 case AIR:
                     break;
@@ -131,7 +139,7 @@ int main()
 
         // Draw state
         window.clear(sf::Color::Black);
-        for (auto i = 0; i < SCREEN_X * SCREEN_Y; i++)
+        for (auto i = 0; i < SANDBOX_X * SANDBOX_Y; i++)
         {
             switch (point_data[i].type)
             {
